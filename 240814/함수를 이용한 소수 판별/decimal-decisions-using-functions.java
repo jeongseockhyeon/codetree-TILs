02811b31de08352ea65f502c0bea98382137b8ad
jeongseockhyeon <113079762+jeongseockhyeon@users.noreply.github.com>
2024-08-14 22:45:27 +0900

    Title: 함수를 이용한 소수 판별, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240814/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240814/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -12,6 +12,9 @@ public class Main {
         return sum;
     }
     public static boolean isPrime(int n){
+        if (n == 1){
+            return false;
+        }
         for(int i = 2; i < n; i++){
             if(n % i == 0){
                 return false;
