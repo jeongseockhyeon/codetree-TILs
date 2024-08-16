89a5f968c9af07cac2e6c35f32db8b7096429d5f
jeongseockhyeon <113079762+jeongseockhyeon@users.noreply.github.com>
2024-08-16 23:03:26 +0900

    Title: 최소 최대, Time: 1940ms, Memory: 104MB, Status: Passed - CodeTree

--- 240816/최소 최대/min-max.java
+++ 240816/최소 최대/min-max.java
@@ -1,21 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static int Max(int[] numArr){
-        int max = numArr[0];
-        for(int i = 0; i < numArr.length; i++){
-            if(max < numArr[i]) max = numArr[i];
-        }
-        return max;
-    }
 
-    public static int Min(int[] numArr){
-        int min = numArr[0];
-        for(int i = 0; i < numArr.length; i++){
-            if(min > numArr[i]) min = numArr[i];
-        }
-        return min;
-    }
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -26,6 +12,8 @@ public class Main {
         for(int i = 0; i < n; i++){
             numArr[i] = sc.nextInt();
         }
-        System.out.print(Min(numArr)+" "+Max(numArr));
+
+        Arrays.sort(numArr);
+        System.out.print(numArr[0]+" "+numArr[n-1]);
     }
 }
