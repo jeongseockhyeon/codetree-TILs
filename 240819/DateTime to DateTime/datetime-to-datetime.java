822738e60e15fb85ab1bcfcb74fe785782dd4c8a
jeongseockhyeon <113079762+jeongseockhyeon@users.noreply.github.com>
2024-08-19 21:12:36 +0900

    Title: DateTime to DateTime, Time: 3013ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240819/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int day = 11;
+        int hour = 11;
+        int min = 11;
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int elapsedTime = 0;
+
+        while(true){
+            if(day == a && hour == b && min == c) break;
+
+            elapsedTime++;
+            min++;
+            if(min == 60){
+                hour++;
+                min = 0;
+            }
+            if(hour == 24){
+                day++;
+                hour = 0;
+            }
+        }
+
+        System.out.println(elapsedTime);
+
+
+    }
+}
